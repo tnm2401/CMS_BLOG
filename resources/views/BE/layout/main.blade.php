b86e785cdd226a6a1f433bc7ff052664748aa8ca
shin520 <adtiennguyen21@gmail.com>
2021-05-20 09:46:01 +0700

    cat temp admin dashboard

---
+++ resources/views/BE/layout/main.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@yield('pagetitle')</title>
+
+  @yield('linkcss')
+
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div>
+
+  @include('BE.layout.header')
+
+  @include('BE.layout.menu')
+
+  @yield('maincontent')
+
+  
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+    @yield('linkjs')
+</body>
+</html>
